4b31d1aac7201781051cc457620bcf58d8cf5b8d
Gushenp <gustavohenriquep861programer@gmail.com>
2026-04-17 21:53:02 -0300

    Escolha de tentativas adicionadas

--- src/login/com/joptionpane/LoginComJOptionPane.java
+++ src/login/com/joptionpane/LoginComJOptionPane.java
@@ -12,24 +12,53 @@ public class LoginComJOptionPane {
     }
 
     public static void _login(){
-       JOptionPane.showMessageDialog(null, "Seja bem vindo !!!");
-       String login = JOptionPane.showInputDialog("Digite seu login");
-       String senha = JOptionPane.showInputDialog("Digite sua senha");
-       
+
        String mensagem = "";
+       int escolha = 0;
+       int tentativasMaximas = 0;
        
-       if (!login.equals("admin")){
-           mensagem = "Usuário não encontrado, tente novamente!";
-       } else if (!senha.equals("1313")){
-           mensagem = "Senha incorreta! Tente novamente!";
-       } else{
-           mensagem = "Login realizado com sucesso!";
-       }
-       
-       _tela_de_aviso(mensagem);
+       do {
+            
+            String login = JOptionPane.showInputDialog("Digite seu login");
+            String senha = JOptionPane.showInputDialog("Digite sua senha");
+            if (!login.equals("admin") && !senha.equals("1313")){
+                mensagem = "Login ou senha inválidos! Tentar novmente?";
+                escolha = _verificar_escolha_usuario(mensagem);
+            } 
+            
+            if (login.equals("admin") && senha.equals("1313")){
+                mensagem = "Login realizado! Bem vindo!";
+                escolha = 1;
+                _tela_de_aviso(mensagem);
+            }
+            
+            tentativasMaximas++;
+            if (tentativasMaximas > 3){
+                mensagem = "Tentativas Máximas! Tente mais tarte!";
+                _tela_de_aviso(mensagem);
+                escolha = 1;
+            }
+            
+       } while(escolha == 0);
+
     }
     
     public static void _tela_de_aviso(String mensagem){
         JOptionPane.showMessageDialog(null, mensagem);
+    }
+    
+    public static int _verificar_escolha_usuario(String mensagem){
+        String[] opcoes = {"Continuar", "Sair"};
+
+        int escolha = JOptionPane.showOptionDialog(
+                null,
+                mensagem,
+                "Escolha opções",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                opcoes,
+                opcoes[0]);    
+        return escolha;
     }
 }
